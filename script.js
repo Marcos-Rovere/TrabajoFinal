952acb6a540f970d30465b22ac083406823046e2
Marcos-Rovere <75751294+Marcos-Rovere@users.noreply.github.com>
2021-02-17 19:54:44 -0300

    Add files via upload
    Agregue el trabajo nuevo

--- script.js
+++ script.js
@@ -1,138 +1,143 @@
+const cotizarPlan=()=> {
+    let nombre = $("#defaultContactFormName").val()
+    let telefono = $("#telefono").val()
+    let mail = $("#defaultContactFormEmail").val()
+    let edad = $("#EdadSocio").val()
+    let plan = $("#PlanElegido").val()
+    let estadoCivil = $("#estadoCivil").val()
+    let hijos = $("#hijos").val()
 
-var plan = document.getElementById("planElegido").value;
+    let divResumen = document.getElementById("resumen")
+    let divResultado = document.getElementById("resultado")
 
-var nombre = document.getElementById("defaultContactFormName").value
+    if (edad === '' || plan === '0' || nombre === ''|| telefono === '' || mail === ''|| hijos === ''){
+        mostrarError ("#errorAlCompletar","Selecciono mal un dato")
+        return
+    }
 
-var edad
-edad = parseInt(prompt("Ingrese su edad"))
+    let cotizacion = {edad,plan,hijos,estadoCivil};
+    
+    
 
-var edadSocio = document.getElementById("EdadSocio")
-console.log(edadSocio)
+    divResumen.style.backgroundColor="#FFF"
+    divResumen.style.display="block"
+    divResumen.innerHTML='<div style="text-align:center"><img src="cargando.gif" width=100 height=100></div>';
+    setTimeout (()=> {
+        divResumen.style.backgroundColor="#00838F"
+        divResumen.innerHTML=`<h2>Resumen de Consulta</h2>
+        <ul>
+            <li>Nombre y Apellido: ${mayuscula(nombre)}</li>
+            <li>Edad: ${mayuscula(edad)}</li>
+            <li>Plan Elegido: ${mayuscula(plan)}</li>
+            <li>Estado Civil: ${mayuscula(estadoCivil)}</li>
+            <li>Hijos: ${mayuscula(hijos)}</li>
+        </ul>`;
+        let cotizacionFinal = cotizar(cotizacion);
+        divResultado.style.display="block";
+        divResultado.className="divResultado"
+        divResultado.innerHTML=`<p class="textoCotizacion">$ ${cotizacionFinal} </p>`;
+    },1500);
 
-edadSocio.value = edad
+    
 
-
-// let casado
-// casado = prompt("esta casado")
-
-
-// let array25Solo = [
-//     "plan: Exclusivo C, precio: 1000",
-//     "plan: Exclusvio, precio: 2000",
-//     "plan: Premium C, precio: 3000",
-//     "plan: Premium, precio: 4000",
-//     "plan: Live, precio: 5000", 
-// ]
-// let array37Solo = [
-//     "plan: Exclusivo C, precio: 2000",
-//     "plan: Exclusvio, precio: 3000",
-//     "plan: Premium C, precio: 4000",
-//     "plan: Premium, precio: 5000",
-//     "plan: Live, precio: 6000", 
-// ]
-// let array50Solo = [
-//     "plan: Exclusivo C, precio: 3000",
-//     "plan: Exclusvio, precio: 4000",
-//     "plan: Premium C, precio: 5000",
-//     "plan: Premium, precio: 6000",
-//     "plan: Live, precio: 7000", 
-// ]
-// let arrayMas50Solo = [
-//     "plan: Exclusivo C, precio: 4000",
-//     "plan: Exclusvio, precio: 5000",
-//     "plan: Premium C, precio: 6000",
-//     "plan: Premium, precio: 7000",
-//     "plan: Live, precio: 8000", 
-// ]
-// let array25Casado = [
-//     "plan: Exclusivo C, precio: 2000",
-//     "plan: Exclusvio, precio: 3000",
-//     "plan: Premium C, precio: 4000",
-//     "plan: Premium, precio: 5000",
-//     "plan: Live, precio: 6000", 
-// ]
-// let array37Casado = [
-//     "plan: Exclusivo C, precio: 3000",
-//     "plan: Exclusvio, precio: 4000",
-//     "plan: Premium C, precio: 5000",
-//     "plan: Premium, precio: 6000",
-//     "plan: Live, precio: 7000", 
-// ]
-// let array50Casado = [
-//     "plan: Exclusivo C, precio: 4000",
-//     "plan: Exclusvio, precio: 5000",
-//     "plan: Premium C, precio: 6000",
-//     "plan: Premium, precio: 7000",
-//     "plan: Live, precio: 8000", 
-// ]
-// let arrayMas50Casado = [
-//     "plan: Exclusivo C, precio: 5000",
-//     "plan: Exclusvio, precio: 6000",
-//     "plan: Premium C, precio: 7000",
-//     "plan: Premium, precio: 8000",
-//     "plan: Live, precio: 9000", 
-// ]
-
-// if (casado == "si"){
-//     casado = true;
-// }
-// else {
-//     casado = false;
-// }
-
-// if (edad < 26 && casado == false){
-//     for (let i = 0; i < array25Solo.length; i++){
-//         console.log(array25Solo[i])
-//     }
-// }
-// else if (edad < 37 && casado == false){
-//     for (let i = 0; i < array37Solo.length; i++){
-//         console.log(array37Solo[i])
-//     }
-// }
-// else if (edad < 50 && casado == false) {
-//     for (let i = 0; i < array50Solo.length; i++){
-//         console.log(array50Solo[i])
-//     }
-// }
-// else if (edad > 50 && casado == false){
-//     for (let i = 0; i < arrayMas50Solo.length; i++){
-//         console.log(arrayMas50Solo[i])
-//     }
-// }
-// else if (edad < 26 && casado == true){
-//     for (let i = 0; i < array25Casado.length; i++){
-//         console.log(array25Casado[i])
-//     }
-// }
-// else if (edad < 37 && casado == true){
-//     for (let i = 0; i < array37Casado.length; i++){
-//         console.log(array37Casado[i])
-//     }
-// }
-// else if (edad < 50 && casado == true){
-//     for (let i = 0; i < array50Casado.length; i++){
-//         console.log(array50Casado[i])
-//     }
-// }
-// else if (edad < 37 && casado == true){
-//     for (let i = 0; i < arrayMas50Casado.length; i++){
-//         console.log(arrayMas50Casado[i])
-//     }
-// }
-// else {
-//     console.log ("Ingrese correctamente la edad");
-// }
-
-const arrayProductos = [
-    {plan: "Exclusivo", precio: 1000},
-    {plan: "Premium", precio: 2000},
-]
-if (plan == "Exclusivo"){
-    // let ArrayFiltrado = arrayProductos.filter ((el) => el.plan == "Exclusivo")
-    // console.log(ArrayFiltrado)
-    console.log(arrayProductos.filter ((el) => el.plan == "Exclusivo"))
 }
-else {
-    console.log ("ingrese el plan correctamente")
+
+function cotizar(cotizacion){
+    const {edad, plan, hijos,estadoCivil} =cotizacion;
+    let  resultado = 2000;
+
+    resultado = calcularEdad(edad)*resultado
+    resultado = calcularPlan(plan)*resultado
+    resultado = calcularEstadoCivil(estadoCivil)*resultado
+    resultado = calcularHijos(hijos)*resultado
+    return resultado
 }
+function calcularPlan(plan){
+    var incrementoPlan
+    if (plan === "Exclusivo C") {
+        incrementoPlan = 1;
+    }
+    else if (plan === "Exclusivo") {
+        incrementoPlan = 1.35
+    }
+    else if (plan === "Premium C") {
+        incrementoPlan = 1.45
+    }
+    else if (plan === "Premium") {
+        incrementoPlan = 1.75
+    }
+    else if (plan === "Live") {
+        incrementoPlan = 2
+    }
+    return incrementoPlan
+}
+
+function calcularEdad(edad){
+    var incrementoEdad
+    if (edad<26) {
+        incrementoEdad = 1
+    }
+    else if (edad < 37 && edad >25) {
+        incrementoEdad = 1.30
+    }
+    else if (edad < 50 && edad >36) {
+        incrementoEdad = 1.6
+    }
+    else if (edad > 49) {
+        incrementoEdad = 1.9
+    }
+    return incrementoEdad
+}
+
+function calcularEstadoCivil(estadoCivil){
+    var incrementoEstadoCivil
+    if (estadoCivil === "Soltero") {
+        incrementoEstadoCivil = 1
+    }
+    else if (estadoCivil === "Casado") {
+        incrementoEstadoCivil = 1.85
+    }
+    else if (estadoCivil === "Concubinato") {
+        incrementoEstadoCivil = 1.85
+    }
+    else if (estadoCivil === "Divorciado") {
+        incrementoEstadoCivil = 1
+    }
+    else if (estadoCivil === "Viudo") {
+        incrementoEstadoCivil = 1
+    }
+    return incrementoEstadoCivil
+}
+
+
+function calcularHijos (hijos){
+    var incrementoHijos
+    if (hijos === "1") {
+        incrementoHijos = 1;
+    }
+    else if (hijos === "2") {
+        incrementoHijos = 1.5
+    }
+    else if (hijos === "3") {
+        incrementoHijos = 2
+    }
+    else if (hijos=== "4") {
+        incrementoHijos = 2.3
+    }
+    else if (hijos === "5") {
+        incrementoHijos = 2.5
+    }
+    return incrementoHijos
+}
+
+function mayuscula(palabra) {
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1)
+}
+
+function mostrarError(elemento, mensaje) {
+    divError = document.querySelector(elemento)
+    divError.innerHTML = `<p class="alert alert-danger error">${mensaje}</p>`
+    setTimeout(() => { divError.innerHTML = '' }, 2500)
+}
+
+$("#calcular").click(cotizarPlan)
